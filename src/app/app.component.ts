5176d6e4a31b2cf2f288d9bef3a4d315ab8c21e5
beqirajd <dejvis.beqiraj@linksmt.it>
2023-06-26 15:39:04 +0200

    update template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,16 @@ import { HomeComponent } from './home/home.component';
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent],
-  template: `<h1>Hello world!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+     </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
